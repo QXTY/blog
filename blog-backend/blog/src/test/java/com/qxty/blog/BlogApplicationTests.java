74ac63e59c711ee1673416a3579f7de3cf8d3e9f
QXTY <468539746@qq.com>
2022-12-13 01:44:30 +0800

    create file directory

---
+++ blog-backend/blog/src/test/java/com/qxty/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qxty.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
